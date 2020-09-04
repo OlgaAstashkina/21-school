65bd514e1b7e9fe5aac9aac31cf70c555f029039
DenKrylov <monie@mi-s1.msk.21-school.ru>
2020-09-04 20:05:57 +0300

    add libft and create project cub3d

---
+++ ring03/cub3d/cub3d.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: monie <monie@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/04 18:55:10 by monie             #+#    #+#             */
+/*   Updated: 2020/09/04 18:55:13 by monie            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *str, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+int		get_next_line(int fd, char **line);
+
+#endif
